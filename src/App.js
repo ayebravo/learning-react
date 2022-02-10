ac30288831e08296a85c743035057e6393f904a9
ayebravo <ayelenbravo@gmail.com>
2022-02-10 14:10:07 -0600

    Showing CSS-in-JS techniques

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import "./App.css";
 import Greeting from "./Greeting";
 
 const App = () => {
+	// CSS in JS -> Styling React components
+	// This can be good to have dynamic styling
+	const styles = {
+		container: {
+			padding: 30,
+			backgroundColor:
+				new Date().getSeconds() % 2 === 0 ? "lightblue" : "pink",
+			heading: {
+				textTransform: "uppercase",
+			},
+		},
+	};
+
 	return (
 		// <></> React fragment -> To serve a single parent element that doesn't render anything
-		<>
-			<h1>Hello React!</h1>
+		<div style={styles.container}>
+			<h1 style={styles.container.heading}>Hello React!</h1>
 			<hr></hr>
 			<Greeting
 				name="Larry"
@@ -14,7 +27,7 @@ const App = () => {
 				favoriteColors={["Red", "Green", "Blue", "Red"]}
 			/>
 			<Greeting name="Sam" favoriteNumber={42} />
-		</>
+		</div>
 	);
 };
 
